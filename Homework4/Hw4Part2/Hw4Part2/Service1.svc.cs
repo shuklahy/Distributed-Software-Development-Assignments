5b66d7cc421109959b0f6342c2af610b7c0b8076
hyshukla <hyshukla@asu.edu>
2017-04-22 05:40:33 -0700

    hw 4 part2 done

---
+++ Homework4/Hw4Part2/Hw4Part2/Service1.svc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+namespace Hw4Part2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static string error = "";
+        private static void validate(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+                error += " Warning" + e.Message;
+            else // Error
+                error += " Error message" + e.Message;
+        }
+
+        result IService1.verification(string xmlPath, string xsdPath)
+        {
+            result output = new result();
+            output.status = "No Error";
+            output.errorMsg = "";
+            output.value = "";
+
+            error = "";
+            try
+            {
+                easyValidate(xmlPath, xsdPath);
+            }
+            catch (Exception err)
+            {
+                error += err.Message;
+            }
+
+            if (!error.Equals(""))
+            {
+                output.status = "Error";
+                output.errorMsg = error;
+            }
+
+            return output;
+        }
+
+
+        
+        result IService1.XPathSearch(string xmlPath, string expression)
+        {
+            result output = new result();
+            output.status = "No Error";
+            output.errorMsg = "";
+            output.value = "";
+
+            XPathDocument dx = new XPathDocument(xmlPath);
+            XPathNavigator nav = dx.CreateNavigator();
+
+            
+            XPathNodeIterator iterator = nav.Select(expression);
+            while (iterator.MoveNext())
+            {
+                output.value += iterator.Current.Value + "\n";
+            }
+            return output;
+        }
+
+
+        /*
+         * Helper Functions
+         */
+
+
+        public static void easyValidate(string xmlPath, string xsdPath)
+        {
+            
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add(null, xsdPath);
+                settings.ValidationType = ValidationType.Schema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+                settings.IgnoreWhitespace = true;
+
+                XmlReader xmldoc = XmlReader.Create(xmlPath, settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(xmldoc);
+
+                ValidationEventHandler eventHandler = new ValidationEventHandler(validate);
+
+                // the following call to Validate succeeds.
+                document.Validate(eventHandler);
+                
+            }
+            
+        }
+        
+        
+    }
+
---
+++ Homework4/Hw4Part2/xmlApp/WebForm2.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="xmlApp.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<style type="text/css">
+        .auto-style1 {
+            text-align: left;
+        }
+        .auto-style2 {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+        <span class="auto-style2"><strong>XmlPathEvaluator</strong></span><br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+        URL for the XML<br />
+        <asp:TextBox ID="TextBox3" runat="server" Width="465px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        XPath Expression<br />
+        <asp:TextBox ID="TextBox5" runat="server" Width="465px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox4" runat="server" Height="142px" ReadOnly="True" TextMode="MultiLine" Width="1066px"></asp:TextBox>
+        <br />
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Evaluate" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
